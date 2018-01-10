8bd1e865bc661e1550aadb9e3cc7e6664f87e78b
Unknown <rixtrevizo@gmail.com>
2018-01-09 19:56:23 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    std::string name;
+    printf("What's ur name?");
+    scanf("%s", name);
+    printf("Hello %s", name);
+}
